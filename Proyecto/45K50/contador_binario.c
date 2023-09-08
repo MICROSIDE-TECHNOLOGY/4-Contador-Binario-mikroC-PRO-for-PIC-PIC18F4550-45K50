6954e0024dfeefc781f8c889e7632ba5255efa71
vhperaltaj <victor.peralta@microside.com>
2023-09-08 10:28:45 -0600

    Fixed indentation

--- Proyecto/45K50/contador_binario.c
+++ Proyecto/45K50/contador_binario.c
@@ -16,26 +16,25 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 2) Project> Edit Project> Insertar 48000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
-#pragma orgall 0x1FFF           //Espacio reservado para bootloader
+#pragma orgall 0x1FFF     // Espacio reservado para bootloader
 
-#define Boton PORTA.f2          //Funcion quedetermina el estado lógico de un boton
+#define Boton PORTA.f2    // Funcion quedetermina el estado lógico de un boton
 
-int cont = 0;                   //Contador de proposito general
+int cont = 0;             // Contador de proposito general
 
-void main() org 0x2000          //vector de inicio usando bootloader MICROSIDE
+void main() org 0x2000    // vector de inicio usando bootloader MICROSIDE
 {
-    TRISA2_bit = 1;                        //Se declara como un pin de entrada
-    TRISB = 0x00;               //Puerto B como salida
-    PORTB = 0X00;               //Puerto B en estado bajo
+   TRISA2_bit = 1;        // Se declara como un pin de entrada
+   TRISB = 0x00;          // Puerto B como salida
+   PORTB = 0X00;          // Puerto B en estado bajo
 
-    while(1){
-        if (Boton==0) {         //Pregunta el estado del boton
-           cont++;
-           PORTB = cont;        //Despliega el valor a PORTB
-           Delay_ms(5);
-           while (Boton==0);
-           Delay_ms(5);
-        }
-    }
-
+   while ( 1 ) {
+      if ( Boton == 0 ) { // Pregunta el estado del boton
+         cont++;
+         PORTB = cont;    // Despliega el valor a PORTB
+         Delay_ms( 5 );
+         while ( Boton == 0 );
+         Delay_ms( 5 );
+      }
+   }
 }
